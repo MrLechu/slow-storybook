082a02c7cf78b4edb11bc2e34bd04832499ba6f4
Lechu <cybulski.leszek@gmail.com>
2021-12-06 19:29:12 +0100

    refactor

--- .history/assets/css/components/Foo_20211206164542.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-
-
-ReactDOM.render(
-    <App></App>
-    document.getElementById('foo')
-)
--- .history/assets/css/components/Foo_20211206164853.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const Foo = () => <div className="test">foo bar</div>;
-
-ReactDOM.render(<Foo />, document.getElementById("foo"));
-
-export default Foo;
